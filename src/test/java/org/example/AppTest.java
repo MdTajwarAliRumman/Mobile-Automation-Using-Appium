026b7f3ec2ba2da7272415571f6b9311f0cdf3b0
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-03-02 10:52:49 +0600

    Setting up tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,29 +1,24 @@
 package org.example;
 
-import org.testng.annotations.BeforeMethod;
-
-public class AppTest {
-    // This sample code supports Appium Java client >=9
-// https://github.com/appium/java-client
-import io.appium.java_client.remote.options.BaseOptions;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Base64;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
+import io.appium.java_client.remote.options.BaseOptions;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-    public class SampleTest {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class AppTest {
 
         private AndroidDriver driver;
 
-        @BeforeEach
-        public void setUp() {
+        @BeforeMethod
+        public void setUp() throws MalformedURLException {
             Capabilities options = new BaseOptions()
                     .amend("platformName", "android")
                     .amend("appium:automationName", "UiAutomator2")
@@ -35,26 +30,51 @@ import org.openqa.selenium.*;
                     .amend("appium:nativeWebScreenshot", true)
                     .amend("appium:newCommandTimeout", 3600)
                     .amend("appium:connectHardwareKeyboard", true);
+            URL remoteURL = new URL("http://127.0.0.1:4723");
 
-            driver = new AndroidDriver(this.getUrl(), options);
+            driver = new AndroidDriver(remoteURL, options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
 
         @Test
         public void sampleTest() {
-
+            WebElement el1 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.view.View\").instance(3)"));
+            el1.click();
+            WebElement el2 = driver.findElement(AppiumBy.accessibilityId("Sign Up"));
+            el2.click();
+            WebElement el3 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(0)"));
+            el3.sendKeys("Tajwar");
+            WebElement el4 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Tajwar\")"));
+            el4.click();
+            WebElement el5 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(0)"));
+            el5.click();
+            el5.sendKeys("Tajwar");
+            WebElement el6 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(1)"));
+            el6.click();
+            el6.sendKeys("volameh289@dolofan.com");
+            WebElement el7 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(2)"));
+            el7.click();
+            el7.sendKeys("01933954158");
+            WebElement el8 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(3)"));
+            el8.click();
+            el8.sendKeys("volameh289@dolofan.com");
+            WebElement el9 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"••••••••••••••••••••••\")"));
+            el9.click();
+            WebElement el10 = driver.findElement(AppiumBy.className("android.widget.ImageView"));
+            el10.click();
+            WebElement el11 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(3)"));
+            el11.click();
+            el11.sendKeys("12345678");
+            WebElement el12 = driver.findElement(AppiumBy.className("android.widget.CheckBox"));
+            el12.click();
+            WebElement el13 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().description(\"Sign Up\").instance(1)"));
+            el13.click();
         }
 
-        @AfterEach
+        @AfterMethod
         public void tearDown() {
             driver.quit();
         }
 
-        private URL getUrl() {
-            try {
-                return new URL("http://127.0.0.1:4723");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-        }
     }
-}
+
